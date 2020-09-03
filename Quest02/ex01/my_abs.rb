03f7f8d187ed97f94b7afce3a23aabe5e0246a0e
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-03 12:45:47 +0100

    array of numbers

--- Quest02/ex01/my_abs.rb
+++
@@ -1,5 +0,0 @@
-#returns absolute number
-def my_abs(nbr)
-    return (nbr).abs
-  end
-  puts my_abs(-5)
---
+++ Quest02/ex01/my_array_numbers.rb
@@ -0,0 +1,6 @@
+#return a range of numbers (0 to 14)
+def my_array_numbers()
+    return (0..14).to_a
+  end
+  
+  print my_array_numbers() , "\n"
