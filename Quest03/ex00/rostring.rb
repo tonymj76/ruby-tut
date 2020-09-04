7e26c16921422bbf4132f7d4d28513e60a056045
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-04 16:04:37 +0100

    fix

--- Quest03/ex00/rostring.rb
+++ Quest03/ex00/rostring.rb
@@ -1,11 +1,9 @@
 # 
 def rostring(string)
-    if string.strip.empty?
-      return ""
-    end
-    h = string.match /(?<first>\w+) (?<rest>.*)/
-    rests = h[:rest].split()
-    return rests.append(h[:first]).join(' ')
+  return "" if string.strip.empty?
+  h = string.match /(?<first>\w+) (?<rest>.*)/
+  rests = h[:rest].split()
+  return rests.append(h[:first]).join(' ')
   end
   
   print rostring(" AkjhZ zLKIJz , 23y"), "\n"
