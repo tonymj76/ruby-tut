0628b0ac2b2eda5ce6c7dcc60fe06cde51d8613c
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-07 12:54:54 +0100

    how to read CSV file

---
+++ Quest04/ex03/my_csv_parser.rb
@@ -0,0 +1,8 @@
+require "csv"
+def my_csv_parser(filename, separator="")
+  array_rows = CSV.read(filename)
+  rows = separator == "" ? array_rows : array_rows
+  print rows
+end
+
+my_csv_parser("./data.csv", )
