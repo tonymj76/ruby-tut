f16cb63e955eaf8ffd9500adebeb01108b41e7fa
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-15 12:42:03 +0100

    MAKING use of getters

--- myEx/classes/getter_settings.rb
+++ myEx/classes/getter_settings.rb
@@ -1,12 +1,13 @@
 class WhatIsYourName
   attr_accessor :name # an easy where to add getter and setting to class object
   def speak 
-    p "say something"
+    "#{name} said something"
   end
 end
 
 dog = WhatIsYourName.new
 dog.name = "bingo"
 p dog.name
+p dog.speak
 #attr_writer
 #attr_reader
