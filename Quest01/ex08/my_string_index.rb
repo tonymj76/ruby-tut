69a3ec89cf8ee090d54cc9e7308780f763c561dc
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-17 15:19:05 +0100

    string index

--- Quest01/ex08/my_string_index.rb
+++ Quest01/ex08/my_string_index.rb
@@ -1,6 +1,10 @@
 #get index of a string
 def my_string_index(haystack, needle)
-    return haystack.index(needle)
+    num = haystack.index(needle)
+    if num.to_i > 0 
+        return num 
+    end
+    return -1
 end
 
 puts my_string_index("hello", "ll")
