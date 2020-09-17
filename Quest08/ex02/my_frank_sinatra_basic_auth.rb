ff9ccd80e2b614ef1e6eee72084393d80d446f58
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-17 14:28:44 +0100

    auth

--- Quest08/ex02/my_frank_sinatra_basic_auth.rb
+++ Quest08/ex02/my_frank_sinatra_basic_auth.rb
@@ -4,11 +4,25 @@ require "sinatra/reloader" if development?
 set :bind, '0.0.0.0'
 set :port, 8080
 
+# helpers do
+#   def base_url
+#     @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['REMOTE_USER']}#{request.env['HTTP_HOST']}"
+#   end
+# end
+
 get('/'){
   "Everybody can see this page"
-  "#{request.url}"
+  # " jid #{request.env}"
 }
 
+# get('/protected') {
+#   url = request.url
+#   if name == 'admin' and password =='admin'
+#     "Welcome, authenticated client"
+#   else
+#     "401 Not authorized."
+#   end
+# }
 get('/protected/:name/:password') { |name, password|
   if name == 'admin' and password =='admin'
     "Welcome, authenticated client"
