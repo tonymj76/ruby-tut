686b2117017cc1146492de73b59313b0d3d7b088
Anthony Anonde <anthony.anonde@tog.ng>
2020-09-23 12:57:33 +0100

    improved

--- Quest04/ex00/my_hamming_dna.rb
+++ Quest04/ex00/my_hamming_dna.rb
@@ -1,5 +1,6 @@
 #difference between two DNA strands
 def hamming_dna(dna_1, dna_2)
+  return -1 if dna_1.size != dna_2.size
   counter = 0
   dna_1.bytes.each_with_index{
     |byte, i| counter +=1 if byte != dna_2.bytes[i]
@@ -7,3 +8,6 @@ def hamming_dna(dna_1, dna_2)
   return counter
 end
 puts hamming_dna("", "")
+puts hamming_dna("GGACGGATTCTG", "AGG")
+puts hamming_dna("ACCAGGG", "ACTATGG")
+puts hamming_dna("GGACTGA", "GGACTGA")
